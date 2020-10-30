277a82904c57bfeb69f005dd657dedefa083f993
Parsa Temple Owl <tug81042@temple.edu>
2020-10-30 15:44:49 -0400

    Added the correct configurations for user registration and login

--- application/__init__.py
+++ application/__init__.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 import os
 from application.VideoStream.VideoFeed import VideoStream
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import re
 
 db = SQLAlchemy()
 root_directory = os.path.abspath(os.path.join(os.getcwd(), 'application'))
@@ -19,17 +16,18 @@ video_stream = VideoStream()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'secret-key'
+    
+    app.config['SECRET_KEY'] = 'secret-key-goes-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
-    app.config['MYSQL_DB'] = 'pythonlogin'
     
     db.init_app(app)
     
-    mysql = MySQL(app)
-
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+    
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
     from application.Blueprints.HomePage.home_page import home_page
     from application.Blueprints.Dashboard.dashboard import dashboard
     from application.Blueprints.VideoGallery.video_gallery import video_gallery
