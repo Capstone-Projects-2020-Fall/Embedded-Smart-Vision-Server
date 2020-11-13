f90e075fe7dc311b7d402b33604d20af0329cc38
Parsa Temple Owl <tug81042@temple.edu>
2020-11-13 18:30:36 -0500

    Update models.py

--- application/models.py
+++ application/models.py
@@ -1,3 +1,4 @@
+from flask_login import UserMixin
 from . import db
 
 
@@ -10,3 +11,10 @@ class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     videoID = db.Column(db.Integer, db.ForeignKey('video.id'), nullable=False)
     classification = db.Column(db.String(100), nullable=False)
+
+class User(UserMixin, db.Model)
+        id = db.Column(db.Integer, primary_key=True)
+        email = db.Column(db.String(100), unique=True)
+        username = db.Column(db.String(100), unique=True)
+        password = db.Column(db.String(100))
+        name = db.Column(db.String(1000))
