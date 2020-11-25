6e644d041a086f950127e78507fe8cb16fa2dbb3
Parsa Temple Owl <parsaahmadinasabemran18@gmail.com>
2020-11-25 10:35:42 -0500

    flask app user 1-facor authentication working

--- application/Blueprints/UserLogout/user_logout.py
+++ application/Blueprints/UserLogout/user_logout.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, login_required
 
 
 
-user_logout = Blueprint('user_logout', __name__)
+user_logout = Blueprint('user_logout', __name__, template_folder="templates")
 
 
 
@@ -11,4 +11,8 @@ user_logout = Blueprint('user_logout', __name__)
 @login_required
 def show_user_logout():
 	logout_user()
-	return redirect(url_for('user_login.show_user_login'))
+	return redirect(url_for('user_logout.show_user_logout_confirmation'))
+
+@user_logout.route('/userlogout')
+def show_user_logout_confirmation():
+    return render_template('user_logout.html', current_page='user_logout')
