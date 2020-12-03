e6ad66301964702ea36e66b74bf23c9132287aa7
Parsa Temple Owl <tug81042@temple.edu>
2020-12-03 17:33:59 -0500

    Update WebDriver.py

--- WebDriver.py
+++ WebDriver.py
@@ -1,3 +1,4 @@
+import os
 from application import create_app
 from multiprocessing import Pipe
 from TestStream import TestStream
@@ -8,5 +9,5 @@ from PIL import Image
 def start_webapp(socket_connection):
     app, socketio = create_app()
     socket_interface = SocketInterface(connection=socket_connection)
-
-    socketio.run(app, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='https://embedded-smart-vision-project.herokuapp.com/', port = port)
