be6dfb427d6fb45cdd5ae01990028d029af1ed4d
Parsa Temple Owl <tug81042@temple.edu>
2020-12-05 15:26:53 -0500

    Update WebDriver.py

--- WebDriver.py
+++ WebDriver.py
@@ -7,7 +7,13 @@ from PIL import Image
 
 
 def start_webapp():
-    app, socketio = create_app()
+    # app, socketio = create_app()
     # socket_interface = SocketInterface(connection=socket_connection)
+    
+    from application import db, create_app
+    app, socket_io = create_app()
+    app.app_context().push()
+    db.create_all()
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port = port)
