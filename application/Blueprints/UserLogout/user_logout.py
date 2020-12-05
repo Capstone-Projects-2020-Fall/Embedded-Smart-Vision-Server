7b098b5c7910efd3ae88c81bf7bad814ca6636c6
Parsa Temple Owl <tug81042@temple.edu>
2020-12-05 16:36:57 -0500

    Update user_logout.py

--- application/Blueprints/UserLogout/user_logout.py
+++ application/Blueprints/UserLogout/user_logout.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, redirect, url_for
 from flask_login import login_user, logout_user, login_required
+from application.models import User
 
 
 
@@ -10,9 +11,13 @@ user_logout = Blueprint('user_logout', __name__, template_folder="templates")
 @user_logout.route('/userlogout')
 @login_required
 def show_user_logout():
-	logout_user()
-	return redirect(url_for('user_logout.show_user_logout_confirmation'))
+	return render_template('user_logout.html', current_page='user_logout')
 
-@user_logout.route('/userlogout')
-def show_user_logout_confirmation():
-    return render_template('user_logout.html', current_page='user_logout')
+@user_logout.route('/userlogout', methods=['GET', 'POST'])
+@login_required
+def show_user_logout_post():
+	email = request.form.get('email')
+	user = User.query.filter_by(email=email).first()
+	user.is_authenticated = False
+	logout_user()
+	return redirect(url_for('user_login.show_user_login'))
